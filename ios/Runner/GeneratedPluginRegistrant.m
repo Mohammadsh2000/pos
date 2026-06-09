80dd755deaf66af64afc3bf23bed4a5e852145b6
Mohammadsh2000 <113411298+Mohammadsh2000@users.noreply.github.com>
2026-06-09 17:10:05 +0300

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,70 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<file_picker/FilePickerPlugin.h>)
+#import <file_picker/FilePickerPlugin.h>
+#else
+@import file_picker;
+#endif
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_database/FLTFirebaseDatabasePlugin.h>)
+#import <firebase_database/FLTFirebaseDatabasePlugin.h>
+#else
+@import firebase_database;
+#endif
+
+#if __has_include(<firebase_remote_config/FirebaseRemoteConfigPlugin.h>)
+#import <firebase_remote_config/FirebaseRemoteConfigPlugin.h>
+#else
+@import firebase_remote_config;
+#endif
+
+#if __has_include(<mobile_scanner/MobileScannerPlugin.h>)
+#import <mobile_scanner/MobileScannerPlugin.h>
+#else
+@import mobile_scanner;
+#endif
+
+#if __has_include(<printing/PrintingPlugin.h>)
+#import <printing/PrintingPlugin.h>
+#else
+@import printing;
+#endif
+
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
+#if __has_include(<sqflite_darwin/SqflitePlugin.h>)
+#import <sqflite_darwin/SqflitePlugin.h>
+#else
+@import sqflite_darwin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FilePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FilePickerPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseDatabasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseDatabasePlugin"]];
+  [FirebaseRemoteConfigPlugin registerWithRegistrar:[registry registrarForPlugin:@"FirebaseRemoteConfigPlugin"]];
+  [MobileScannerPlugin registerWithRegistrar:[registry registrarForPlugin:@"MobileScannerPlugin"]];
+  [PrintingPlugin registerWithRegistrar:[registry registrarForPlugin:@"PrintingPlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+}
+
+@end
---
+++ lib/utils/invoice_pdf.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'package:pdf/pdf.dart' as pdf_lib;
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+import 'package:intl/intl.dart';
+import '../constants.dart';
+import '../models/sale_item.dart';
+
+Future<void> generateInvoicePdf(Map<String, dynamic> sale) async {
+  final items = (jsonDecode(sale['items']) as List)
+      .map((e) => SaleItem.fromMap(e as Map<String, dynamic>))
+      .toList();
+  final date = DateTime.parse(sale['created_at'] as String);
+  final pdf = pw.Document();
+  pdf.addPage(
+    pw.MultiPage(
+      pageFormat: pdf_lib.PdfPageFormat.roll80,
+      margin: const pw.EdgeInsets.all(16),
+      build: (pw.Context ctx) {
+        return [
+          pw.Center(
+            child: pw.Text(
+              kStoreName,
+              style: pw.TextStyle(fontSize: 20, fontWeight: pw.FontWeight.bold),
+            ),
+          ),
+          pw.SizedBox(height: 6),
+          pw.Center(
+            child: pw.Text('فاتورة مبيعات', style: pw.TextStyle(fontSize: 14)),
+          ),
+          pw.SizedBox(height: 4),
+          pw.Center(
+            child: pw.Text(
+              DateFormat('yyyy/MM/dd HH:mm:ss').format(date),
+              style: pw.TextStyle(fontSize: 10),
+            ),
+          ),
+          pw.SizedBox(height: 2),
+          pw.Center(
+            child: pw.Text('#${sale['id']}', style: pw.TextStyle(fontSize: 10)),
+          ),
+          pw.Divider(),
+          pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+            children: [
+              pw.Text('المنتج', style: pw.TextStyle(fontWeight: pw.FontWeight.bold)),
+              pw.Text('العدد', style: pw.TextStyle(fontWeight: pw.FontWeight.bold)),
+              pw.Text('المجموع', style: pw.TextStyle(fontWeight: pw.FontWeight.bold)),
+            ],
+          ),
+          pw.Divider(),
+          ...items.map(
+            (i) => pw.Padding(
+              padding: const pw.EdgeInsets.symmetric(vertical: 2),
+              child: pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Text(i.productName, style: pw.TextStyle(fontSize: 10)),
+                  pw.Text(i.quantity.toString(), style: pw.TextStyle(fontSize: 10)),
+                  pw.Text(i.subtotal.toStringAsFixed(2), style: pw.TextStyle(fontSize: 10)),
+                ],
+              ),
+            ),
+          ),
+          pw.Divider(),
+          pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+            children: [
+              pw.Text('المجموع الكلي:', style: pw.TextStyle(fontWeight: pw.FontWeight.bold)),
+              pw.Text(
+                '${sale['total']} ر.س',
+                style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+              ),
+            ],
+          ),
+          pw.SizedBox(height: 20),
+          pw.Center(
+            child: pw.Text('شكراً لتسوقكم معنا', style: pw.TextStyle(fontSize: 10)),
+          ),
+        ];
+      },
+    ),
+  );
+  await Printing.sharePdf(
+    bytes: await pdf.save(),
+    filename: 'فاتورة_${sale['id']}.pdf',
+  );
+}
