4a351a9f556ef9152da080489757bc4ebf2108ad
Mohammadsh2000 <113411298+Mohammadsh2000@users.noreply.github.com>
2026-06-14 14:34:02 +0300

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-// --- متجر المعلومات ---
-String kStoreName = 'متجري';
-String kStoreAddress = '';
-String kStorePhone = '';
-const kStoreFooter = 'نرحب باستفساراتكم';
-
-const kCategories = ['عام', 'مواد غذائية', 'مشروبات', 'إلكترونيات', 'ملابس', 'أدوات منزلية'];
-
-String kCurrencySymbol = 'ر.س';
