acf6b5d222ca74f9cc0ca85cefdfb8e18c0e342c
Mohammadsh2000 <whgptv2004@gmail.com>
2026-07-14 10:36:07 +0300

    Auto update modified files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleSignIn
+import AVFoundation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -25,5 +26,57 @@ import GoogleSignIn
       return true
     }
     return super.application(app, open: url, options: options)
+  }
+}
+
+class SwiftFeedbackPlugin: NSObject, FlutterPlugin {
+  private var audioPlayer: AVAudioPlayer?
+  private var soundData: Data?
+
+  static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(
+      name: "com.example.pos/feedback",
+      binaryMessenger: registrar.messenger()
+    )
+    let instance = SwiftFeedbackPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "isAvailable":
+      result(true)
+    case "loadSound":
+      if let args = call.arguments as? FlutterStandardTypedData {
+        soundData = args.data
+        result(true)
+      } else {
+        result(false)
+      }
+    case "playBeep":
+      playSound(volume: 1.0, rate: 1.0)
+      result(true)
+    case "playError":
+      playSound(volume: 0.8, rate: 0.85)
+      result(true)
+    case "playSuccess":
+      playSound(volume: 1.0, rate: 1.2)
+      result(true)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+
+  private func playSound(volume: Float, rate: Float) {
+    guard let data = soundData else { return }
+    try? AVAudioSession.sharedInstance().setCategory(.playback, options: .mixWithOthers)
+    try? AVAudioSession.sharedInstance().setActive(true)
+    let player = try? AVAudioPlayer(data: data)
+    player?.volume = volume
+    player?.enableRate = true
+    player?.rate = rate
+    player?.prepareToPlay()
+    player?.play()
+    audioPlayer = player
   }
 }
